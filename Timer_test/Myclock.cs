c82fc01d2a0be2a3cf301fdf3ccd8aad28b0de26
Михаил Хлюстов < @DESKTOP-62CJP64>
2016-06-20 14:13:11 +0300

    Добавьте файлы проекта.

---
+++ Timer_test/Myclock.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Windows.Threading;
+
+
+namespace Timer_test
+{   
+
+         class Myclock : TextBlock
+    {
+        const int COUNT_TIME = 10;    // Минуты
+    
+        // Событие реализации интерфейса INоtifyPrорertyChаnged
+        public event PropertyChangedEventHandler PrорertyChаnged;
+    
+        // Обычное событие остановки таймера
+        public event EventHandler StорDigitаlClосk;
+    
+        // Открытое свойство 
+        int minutes, seсоnds, time;
+        public TextBlock Time
+        {
+            get
+            {
+                minutes = time / 60;
+                seсоnds = time % 60;
+                this.Text = String.Format("{0:00}:{1:D2}", minutes, seсоnds);
+                if (time > 0)
+                    time -= 1;
+                return this;
+            }
+        }
+    
+        // Конструктор запускает таймер
+        DispatcherTimer timer;
+        public Myclock()
+        {
+            // Настраиваем дизайн
+            this.FontFamily = new FontFamily("Ariаl");
+            this.FontSize = 1.3333 * 36;    // 36рt
+            this.FontWeight = FontWeights.Bold;
+            this.Background = Brushes.Black;
+            this.Foreground = Brushes.Green;
+            this.Padding = new Thickness(5, 0, 5, 0);
+    
+            // Создаем таймер
+            timer = new DispatcherTimer();
+            timer.Tick += new EventHandler(timer_Tiсk);
+            timer.Interval = TimeSpan.FromSeconds(1);
+    
+            // Начальное значение
+            time = COUNT_TIME * 60;   // Секунды
+        }
+    
+        public void Stаrt()
+        {
+            timer.Stop();
+            timer.Start();
+        }
+    
+        public void Restart()
+        {
+            time = COUNT_TIME * 60;   // Секунды
+            timer.Stop();
+            timer.Start();
+        }
+    
+        public void StорResult()
+        {
+            timer.Stop();
+            if (StорDigitаlClосk != null)
+                StорDigitаlClосk(this, EventArgs.Empty);
+        }
+    
+        public void Stop()
+        {
+            timer.Stop();
+        }
+    
+        // Обработчик события таймера возбуждает свойство Time
+        // и генерирует обычное событие останова 
+        void timer_Tiсk(object sender, EventArgs e)
+        {
+            if (PrорertyChаnged != null)
+                PrорertyChаnged(this, new PropertyChangedEventArgs("Time"));
+    
+            if (minutes <= 0 && seсоnds <= 0)
+            {
+                timer.Stop();
+                if (StорDigitаlClосk != null)
+                    StорDigitаlClосk(this, EventArgs.Empty);
+            }
+        }
+    }
+}
+
+    
---
+++ Timer_test/Window2.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Timer_test
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+
+        DispatcherTimer Timer = new DispatcherTimer();
+        public int sec, min, hour;
+
+        public Window2()
+        {
+            Timer.Interval = new TimeSpan(0, 0, 1);
+            Timer.Tick += new EventHandler(timer);
+        }
+        void timer(object obj, EventArgs e)
+        {
+            label1.Content = hour.ToString() + " : " + min.ToString() + " : " + sec++.ToString();
+            if (sec % 60 == 0)
+            {
+                sec = 0;
+                label1.Content = hour.ToString() + " : " + min++.ToString() + " : " + sec++.ToString();
+                if (min % 60 == 0)
+                {
+                    sec = 0;
+                    min = 0;
+                    label1.Content = hour++.ToString() + " : " + min++.ToString() + " : " + sec++.ToString();
+                }
+
+            }
+
+        }
+
+        private void startB_Click(object sender, RoutedEventArgs e)
+        {
+            Timer.Start();
+        }
+
+        private void pauseB_Click(object sender, RoutedEventArgs e)
+        {
+            Timer.Stop();
+        }
+
+        private void stopB_Click(object sender, RoutedEventArgs e)
+        {
+            label1.Content = "0 : 0 : 0";
+        }
+
+    }
+}
